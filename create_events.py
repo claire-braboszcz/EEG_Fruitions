6df43798eb824963a91879010231dda3b691ac52
Claire Braboszcz <claire@guakamole.org>
2020-12-02 11:07:52 +0000

    init

---
+++ create_events.py
@@ -0,0 +1,47 @@
+import os.path as op
+import os
+import numpy as np
+import mne
+from mne_bids.copyfiles import copyfile_brainvision
+import pandas as pd
+
+
+data_path='/home/claire/Documents/scripts-local/EEG_Fruitions/Data'
+
+
+vhdr_file = op.join(data_path, 'EEG BG2 DMI Day 10.vhdr')
+vhdr_file_renamed = op.join(data_path, 'EEGBG2DMI_Day10.vhdr')
+
+copyfile_brainvision(vhdr_file, vhdr_file_renamed, verbose=True)
+
+
+raw = mne.io.read_raw_brainvision(vhdr_file)
+
+events= pd.read_excel('fruition_blink.xlsx')
+
+#get annotations corresponding to current eeg file
+my_events = events[events['Raw Filename'].str.contains('EEG BG2 DMI Day 10.eeg')]
+
+
+my_events['Location (s)']
+
+my_events
+
+#use mne.read_annotations to create mne.annotation object
+
+my_annot = mne.Annotations(onset = my_events['Location (s)'], 
+                         duration = [1, 1, 1, 1, 1, 1, 1], 
+                         description= ['F','F','F', 'F', 'F', 'F', 'F'])
+
+my_annot
+
+# add annotations to raw object
+raw.set_annotations(my_annot)
+
+print(raw.annotations)
+
+
+events, _ = mne.events_from_annotations(raw, event_id= dict(F=1))
+
+raw.plot(events=events)
+
