87e19a9c65dbe1c07ca624a1e81f671fe5f9f7b7
Claire Braboszcz <claire@guakamole.org>
2020-12-07 22:55:57 +0000

    added new variables

--- config.py
+++ config.py
@@ -14,22 +14,27 @@ adapted from https://github.com/AaltoImagingLanguage/conpy/tree/master/scripts
 @author: claire
 """
 
-
+import os
+import pandas as pd
 from fnames import FileNames
-
 
 
 
 data_path='/home/claire/Documents/scripts-local/EEG_Fruitions/Data'
 
+bids_root = os.path.join(data_path, 'EEG-Fruitions-BIDS')
 
-sessions = [3, 
-            9, 
-            10, 
-            13, 
-            17, 
-            19]
+df_spreadsheet = pd.read_excel((os.path.join(data_path, 'fruition_blink.xlsx')))
 
+tasks = ['fruitions']
+subject_ids = [1]
+
+# define sesssions name from spreadsheet
+sessions=[]
+
+for sess in df_spreadsheet['Raw Filename'].unique():
+    sess= sess.replace('.eeg', '').replace(" ", "_")
+    sessions.append(sess)
 
 
 
